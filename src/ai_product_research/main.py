6c82fd0e48bcc8934b9dbdd78bdc9e589d258782
Vitalii Honchar <weaxme@gmail.com>
2025-12-28 09:05:56 +0100

    Use current date instead of hard coded one

--- src/ai_product_research/main.py
+++ src/ai_product_research/main.py
@@ -9,9 +9,7 @@ log = logging.getLogger(__name__)
 
 async def main():
     ctx = create_app_context()
-
-    # Get posts for December 18, 2025
-    target_date = datetime(2025, 12, 18, 0, 0, 0)
+    target_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     await ctx.telegram_product_research_use_case.execute(target_date)
 
 
